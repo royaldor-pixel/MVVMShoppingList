b5b244ee2a5863ac332cd897f851729b302c7992
royaldor-pixel <spencerzhyp@gmail.com>
2022-08-20 15:55:08 +0800

    database

--- app/src/main/java/com/rd/mvvmshoppinglist/data/db/ShoppingDatabase.kt
+++ app/src/main/java/com/rd/mvvmshoppinglist/data/db/ShoppingDatabase.kt
@@ -8,7 +8,8 @@ import com.rd.mvvmshoppinglist.data.db.entities.ShoppingItem
 
 @Database(
     entities = [ShoppingItem::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
 abstract class ShoppingDatabase : RoomDatabase() {
 
