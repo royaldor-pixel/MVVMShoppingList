a6e9a7a3696ebfa4e2a76dba1feccff0eac96247
royaldor-pixel <spencerzhyp@gmail.com>
2022-08-20 15:27:25 +0800

    database

--- app/src/main/java/com/rd/mvvmshoppinglist/ShoppingItem.kt
+++ app/src/main/java/com/rd/mvvmshoppinglist/ShoppingItem.kt
@@ -9,5 +9,8 @@ data class ShoppingItem(
     @PrimaryKey val gid: Int,
     @ColumnInfo(name = "item_name") val name: String,
     @ColumnInfo(name = "item_amount") val amount: Int,
-    @ColumnInfo(name = "item_price") val price: Double,
-)
+//    @ColumnInfo(name = "item_price") val price: Double,
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}
--- app/src/main/java/com/rd/mvvmshoppinglist/db/ShoppingDao.kt
+++ app/src/main/java/com/rd/mvvmshoppinglist/db/ShoppingDao.kt
@@ -1,12 +1,21 @@
 package com.rd.mvvmshoppinglist.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.rd.mvvmshoppinglist.ShoppingItem
 
 @Dao
 interface ShoppingDao {
-    @Insert(entity = ShoppingItem::class)
-    fun upsert()
+    @Insert(entity = ShoppingItem::class, onConflict = REPLACE)
+    suspend fun upsert(item: ShoppingItem)
+
+    @Delete
+    suspend fun delete(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAllShoppingItems(): LiveData<List<ShoppingItem>>
 }
