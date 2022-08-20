30a6254f52825ed322780d69c77c2a6f0757fd85
royaldor-pixel <spencerzhyp@gmail.com>
2022-08-20 15:52:45 +0800

    database

--- app/src/main/java/com/rd/mvvmshoppinglist/data/db/entities/ShoppingItem.kt
+++ app/src/main/java/com/rd/mvvmshoppinglist/data/db/entities/ShoppingItem.kt
@@ -6,10 +6,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "shopping_items")
 data class ShoppingItem(
-    @PrimaryKey val gid: Int,
     @ColumnInfo(name = "item_name") val name: String,
     @ColumnInfo(name = "item_amount") val amount: Int,
-//    @ColumnInfo(name = "item_price") val price: Double,
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null
